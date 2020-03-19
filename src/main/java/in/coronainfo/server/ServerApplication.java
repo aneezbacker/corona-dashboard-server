05b7a782ea679a0cce2c3cdda10f38beed157dda
Aneez Backer <aneezbacker@gmail.com>
2020-03-19 23:48:47 +0800

    - adding target env - adding file for Docker

--- src/main/java/in/coronainfo/server/ServerApplication.java
+++ src/main/java/in/coronainfo/server/ServerApplication.java
@@ -1,11 +1,16 @@
 package in.coronainfo.server;
 
 import com.googlecode.objectify.ObjectifyService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
+
+    @Value("${TARGET:World}")
+    String target;
+
     public static void main(String[] args) {
         // init Objectify
         ObjectifyService.init();
